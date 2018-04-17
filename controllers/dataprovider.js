7282fb64a2599e5828f5aa006842580015a68e86
hs <hs@spritle.com>
2018-04-17 09:30:44 +0530

    FEAT - Provide students data

---
+++ controllers/dataprovider.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const Student = require('../models/Student');
+
+
+exports.giveStudents = (req, res) => {
+    console.log('got the call from frontend')
+    Student.find({}, (err, students) => {
+        if(err) {console.log(err);}
+        res.send(students);
+    })
+}
