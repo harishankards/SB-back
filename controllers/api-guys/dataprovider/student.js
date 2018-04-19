d8c1006f8ff4ac588934a13d0144b900ff22ef5c
hs <hs@spritle.com>
2018-04-19 06:10:04 +0530

    FIX - disable lusca and solve the csrf issues for now

--- controllers/api-guys/dataprovider/student.js
+++ controllers/api-guys/dataprovider/student.js
@@ -22,40 +22,44 @@ exports.giveStudents = (req, res) => {
  * POST /signup
  * Create a new local account.
  */
+
 exports.postSignup = (req, res, next) => {
-    console.log('received the signup request', req)
-    // req.assert('email', 'Email is not valid').isEmail();
-    // req.assert('password', 'Password must be at least 4 characters long').len(4);
-    // req.assert('confirmPassword', 'Passwords do not match').equals(req.body.password);
-    req.sanitize('email').normalizeEmail({ gmail_remove_dots: false });
+    console.log('received the signup request', req.body) 
+}
+// exports.postSignup = (req, res, next) => {
+//     console.log('received the signup request', req)
+//     // req.assert('email', 'Email is not valid').isEmail();
+//     // req.assert('password', 'Password must be at least 4 characters long').len(4);
+//     // req.assert('confirmPassword', 'Passwords do not match').equals(req.body.password);
+//     req.sanitize('email').normalizeEmail({ gmail_remove_dots: false });
   
-    const errors = req.validationErrors();
+//     const errors = req.validationErrors();
   
-    if (errors) {
-    //   req.flash('errors', errors);
-      return res.send(errors);
-    }
+//     if (errors) {
+//     //   req.flash('errors', errors);
+//       return res.send(errors);
+//     }
   
-    const student = new Student({
-      email: req.body.email,
-      password: req.body.password
-    });
+//     const student = new Student({
+//       email: req.body.email,
+//       password: req.body.password
+//     });
   
-    Student.findOne({ email: req.body.email }, (err, existingUser) => {
-      if (err) { return next(err); }
-      if (existingUser) {
-        req.flash('errors', { msg: 'Account with that email address already exists.' });
-        return res.send('Account with that email address already exists.');
-      }
-      student.save((err, saved) => {
-        if (err) { return next(err); }
-        req.logIn(user, (err) => {
-          if (err) {
-            return next(err);
-          }
-          res.send(saved);
-        });
-      });
-    });
-  };
+//     Student.findOne({ email: req.body.email }, (err, existingUser) => {
+//       if (err) { return next(err); }
+//       if (existingUser) {
+//         req.flash('errors', { msg: 'Account with that email address already exists.' });
+//         return res.send('Account with that email address already exists.');
+//       }
+//       student.save((err, saved) => {
+//         if (err) { return next(err); }
+//         req.logIn(user, (err) => {
+//           if (err) {
+//             return next(err);
+//           }
+//           res.send(saved);
+//         });
+//       });
+//     });
+//   };
   
