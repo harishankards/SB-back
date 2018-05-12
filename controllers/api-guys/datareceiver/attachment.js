08e7480a7c1f62801aee71c672dba2509b7f4298
hs <hs@spritle.com>
2018-05-13 04:45:15 +0530

    CONF - COnfigure S3 bucket storables and objects inside project

--- controllers/api-guys/datareceiver/attachment.js
+++ controllers/api-guys/datareceiver/attachment.js
@@ -5,6 +5,38 @@ const Student = require('../../../models/Student');
 const Company = require('../../../models/Company');
 const jwt = require('jsonwebtoken');
 const fs = require('fs');
+const awsSDK = require('aws-sdk');
+
+
+const serverConfig = {
+  s3: {
+    bucket: 'student-burger',
+    region: 'us-east-1',
+    accessKeyId: 'AKIAITZKXZC3HPB7M4UA',
+    secretAccessKey: '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',
+    defaultExpiry: 86400,
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 exports.createAttachment = (req, res) => {
@@ -35,3 +67,4 @@ exports.deleteAttachment = (req, res) => {
     console.log('deleted', success)
   })
 }
+
