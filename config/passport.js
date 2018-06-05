ec29bb259801a06ef7f3af3b2520d15722a2b5cc
hs <hs@spritle.com>
2018-06-06 04:24:36 +0530

    FEAT - Add new linkedin strategy for company using passport

--- config/passport.js
+++ config/passport.js
@@ -308,7 +308,7 @@ passport.use(new GoogleStrategy({
 /**
  * Sign in with LinkedIn.
  */
-passport.use(new LinkedInStrategy({
+passport.use(student-linkedin, new LinkedInStrategy({
   clientID: process.env.LINKEDIN_ID,
   clientSecret: process.env.LINKEDIN_SECRET,
   callbackURL: process.env.LINKEDIN_CALLBACK_URL,
@@ -316,13 +316,13 @@ passport.use(new LinkedInStrategy({
   passReqToCallback: true
 }, (req, accessToken, refreshToken, profile, done) => {
   if (req.user) {
-    User.findOne({ linkedin: profile.id }, (err, existingUser) => {
+    Student.findOne({ linkedin: profile.id }, (err, existingUser) => {
       if (err) { return done(err); }
       if (existingUser) {
         req.flash('errors', { msg: 'There is already a LinkedIn account that belongs to you. Sign in with that account or delete it, then link it with your current account.' });
         done(err);
       } else {
-        User.findById(req.user.id, (err, user) => {
+        Student.findById(req.user.id, (err, user) => {
           if (err) { return done(err); }
           user.linkedin = profile.id;
           user.tokens.push({ kind: 'linkedin', accessToken });
@@ -339,12 +339,71 @@ passport.use(new LinkedInStrategy({
       }
     });
   } else {
-    User.findOne({ linkedin: profile.id }, (err, existingUser) => {
+    Student.findOne({ linkedin: profile.id }, (err, existingUser) => {
       if (err) { return done(err); }
       if (existingUser) {
         return done(null, existingUser);
       }
-      User.findOne({ email: profile._json.emailAddress }, (err, existingEmailUser) => {
+      Student.findOne({ email: profile._json.emailAddress }, (err, existingEmailUser) => {
+        if (err) { return done(err); }
+        if (existingEmailUser) {
+          req.flash('errors', { msg: 'There is already an account using this email address. Sign in to that account and link it with LinkedIn manually from Account Settings.' });
+          done(err);
+        } else {
+          const user = new User();
+          user.linkedin = profile.id;
+          user.tokens.push({ kind: 'linkedin', accessToken });
+          user.email = profile._json.emailAddress;
+          user.profile.name = profile.displayName;
+          user.profile.location = profile._json.location.name;
+          user.profile.picture = profile._json.pictureUrl;
+          user.profile.website = profile._json.publicProfileUrl;
+          user.save((err) => {
+            done(err, user);
+          });
+        }
+      });
+    });
+  }
+}));
+
+passport.use(company-linkedin, new LinkedInStrategy({
+  clientID: process.env.LINKEDIN_ID,
+  clientSecret: process.env.LINKEDIN_SECRET,
+  callbackURL: process.env.LINKEDIN_CALLBACK_URL,
+  scope: ['r_basicprofile', 'r_emailaddress'],
+  passReqToCallback: true
+}, (req, accessToken, refreshToken, profile, done) => {
+  if (req.user) {
+    Company.findOne({ linkedin: profile.id }, (err, existingUser) => {
+      if (err) { return done(err); }
+      if (existingUser) {
+        req.flash('errors', { msg: 'There is already a LinkedIn account that belongs to you. Sign in with that account or delete it, then link it with your current account.' });
+        done(err);
+      } else {
+        Company.findById(req.user.id, (err, user) => {
+          if (err) { return done(err); }
+          user.linkedin = profile.id;
+          user.tokens.push({ kind: 'linkedin', accessToken });
+          user.profile.name = user.profile.name || profile.displayName;
+          user.profile.location = user.profile.location || profile._json.location.name;
+          user.profile.picture = user.profile.picture || profile._json.pictureUrl;
+          user.profile.website = user.profile.website || profile._json.publicProfileUrl;
+          user.save((err) => {
+            if (err) { return done(err); }
+            req.flash('info', { msg: 'LinkedIn account has been linked.' });
+            done(err, user);
+          });
+        });
+      }
+    });
+  } else {
+    Company.findOne({ linkedin: profile.id }, (err, existingUser) => {
+      if (err) { return done(err); }
+      if (existingUser) {
+        return done(null, existingUser);
+      }
+      Company.findOne({ email: profile._json.emailAddress }, (err, existingEmailUser) => {
         if (err) { return done(err); }
         if (existingEmailUser) {
           req.flash('errors', { msg: 'There is already an account using this email address. Sign in to that account and link it with LinkedIn manually from Account Settings.' });
