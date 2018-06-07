984ed42f6e80172dc366ed322cd3bd632454e504
hs <hs@spritle.com>
2018-06-07 18:59:25 +0530

    DEPLOY - Pack and add the static frontend files

---
+++ public/static/js/9.168c50cb04d14499ce0f.js
@@ -0,0 +1,2 @@
+webpackJsonp([9],{LaPk:function(e,n,t){"use strict";var r=function(){var e=this,n=e.$createElement;return(e._self._c||n)("h3",[e._v("coming soon..")])},l=[],u={render:r,staticRenderFns:l};n.a=u},ocOX:function(e,n,t){"use strict";Object.defineProperty(n,"__esModule",{value:!0});var r=t("LaPk"),l=t("VU/8"),u=l(null,r.a,!1,null,null,null);n.default=u.exports}});
+//# sourceMappingURL=9.168c50cb04d14499ce0f.js.map
