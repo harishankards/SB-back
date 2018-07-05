3bf04cfdbead22c13924bcdc7728ee33074bd60e
hs <hs@spritle.com>
2018-07-06 00:34:54 +0530

    FEAT - Add static files to server

---
+++ public/static/js/10.eecc8598427e699f1dbf.js
@@ -0,0 +1,2 @@
+webpackJsonp([10],{LaPk:function(e,n,t){"use strict";var r=function(){var e=this,n=e.$createElement;return(e._self._c||n)("h3",[e._v("coming soon..")])},l=[],u={render:r,staticRenderFns:l};n.a=u},ocOX:function(e,n,t){"use strict";Object.defineProperty(n,"__esModule",{value:!0});var r=t("LaPk"),l=t("VU/8"),u=l(null,r.a,!1,null,null,null);n.default=u.exports}});
+//# sourceMappingURL=10.eecc8598427e699f1dbf.js.map
---
+++ public/static/js/11.17703774e61e1803bc9b.js
@@ -0,0 +1,2 @@
+webpackJsonp([11],{j8Tx:function(e,n,t){"use strict";Object.defineProperty(n,"__esModule",{value:!0});var r=t("vgOa"),l=t("VU/8"),u=l(null,r.a,!1,null,null,null);n.default=u.exports},vgOa:function(e,n,t){"use strict";var r=function(){var e=this,n=e.$createElement;return(e._self._c||n)("h3",[e._v("coming soon..")])},l=[],u={render:r,staticRenderFns:l};n.a=u}});
+//# sourceMappingURL=11.17703774e61e1803bc9b.js.map
